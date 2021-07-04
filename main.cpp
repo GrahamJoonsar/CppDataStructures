45dcacd079e8b58953ef8552eda65a73fd0ef0ff
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-07-04 16:32:52 -0400

    Removed the Binary tree and added a custom list.
    Added a list class that contains information about the list, and figured out how to define template functions in another file.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,12 @@
 #include <iostream>
+#include "data_structures.h"
 
-// Binary tree
-template <typename T>
-class BinTreeNode {
-    public:
-        // The actual value contained in the node
-        T val; // The val of the node
-        BinTreeNode * left; // The left node
-        BinTreeNode * right; // The right node
-
-        BinTreeNode<T>(T _val); // The constructor
-
-        void AddNode(T _val); // Adding a node to the tree
-};
-
-template <typename T>
-BinTreeNode<T>::BinTreeNode(T _val){
-    val = _val;
-}
-
-template <typename T>
-void BinTreeNode<T>::AddNode(T _val){
-    if (_val < val){ // Going the the left of the current node
-        if (left){ // If left is not a null ptr
-            left = new BinTreeNode(_val);
-        } else {
-            left->AddNode(_val);
-        }
-    } else if (_val > val){ // Going to the right of the current node
-        if (right){ // If right is not a null ptr
-            right = new BinTreeNode(_val);
-        } else {
-            right->AddNode(_val);
-        }
+int main(){
+    IList<int> test(false, 16);
+    for (int i = 0; i < 16; i++){
+        test.add((i + 1) * 9);
     }
-}
-
-int main(void){
-    BinTreeNode<int> intRoot(0);
-    intRoot.AddNode(5);
-    intRoot.right->AddNode(3);
-
-    std::cout << intRoot.val << std::endl;
-    std::cout << intRoot.right->val << std::endl;
-    std::cout << intRoot.right->left->val << std::endl;
-
-    return 0;
+    for (int i = 0; i < 16; i++){
+        std::cout << test.get(i) << std::endl;
+    }
 }
